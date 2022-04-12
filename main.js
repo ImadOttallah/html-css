4c5c160448d6d83c152242012c2b28f95970fc06
Imad Ottallah <imadotta@gmail.com>
2022-04-11 22:36:05 -0500

    added javascript

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Here we go! Week 2! Let's do this");
+//console.log("Here we go! Week 2! Let's do this");
+// SNEAK PEEK
+// document.querySelector('article').innerHTML = 'From JS';
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  // e.preventDefault();
+  console.log("You submitted the form!");
+})
